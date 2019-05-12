a2d6ed0d172efe082ddd91e752711c55b534ce89
mohammedsalah19 <themsm1922011@yahoo.com>
2019-05-12 15:18:22 -0700

    second commit

---
+++ FINALchat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FINALchat.Startup))]
+
+namespace FINALchat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
